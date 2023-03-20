3f4da90077c18a120dc2ce0811b02563f9e4c923
Jaminton Moreno <jaminton.morenof@gmail.com>
2023-03-19 23:15:51 -0500

    Se crea tabla de datos en HeidySql.

--- backend/ventas/src/main/java/com/ventas/ventas/VentasApplication.java
+++ backend/ventas/src/main/java/com/ventas/ventas/VentasApplication.java
@@ -1,11 +1,11 @@
 package com.ventas.ventas;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VentasApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(VentasApplication.class, args);
 	}
---
+++ backend/ventas/src/main/java/com/ventas/ventas/repository/IClienteRepository.java
@@ -0,0 +1,8 @@
+package com.ventas.ventas.repository;
+
+import com.ventas.ventas.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IClienteRepository extends JpaRepository<Cliente, Integer> {
+
+}

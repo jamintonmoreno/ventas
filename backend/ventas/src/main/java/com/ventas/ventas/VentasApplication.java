056b15eaa154249e9772b017ad41da69683273cf
Jaminton Moreno <jaminton.morenof@gmail.com>
2023-03-20 15:42:36 -0500

    Configuracion del entorno de desarrollo FrontEnd

--- backend/ventas/src/main/java/com/ventas/ventas/VentasApplication.java
+++ backend/ventas/src/main/java/com/ventas/ventas/VentasApplication.java
@@ -4,10 +4,15 @@ package com.ventas.ventas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.logging.Logger;
+
 @SpringBootApplication
 public class VentasApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(VentasApplication.class, args);
-	}
+
+
+    public static void main(String[] args) {
+        
+        SpringApplication.run(VentasApplication.class, args);
+    }
 
 }
